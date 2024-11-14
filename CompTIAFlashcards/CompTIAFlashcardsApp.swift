efe1167b0d7a743cf33b37762d52a609c3252ec6
cjordan223 <126746175+cjordan223@users.noreply.github.com>
2024-11-14 10:30:46 -0800

    Initial Commit

---
+++ CompTIAFlashcards/CompTIAFlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompTIAFlashcardsApp.swift
+//  CompTIAFlashcards
+//
+//  Created by connerjordan on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CompTIAFlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompTIAFlashcardsTests/CompTIAFlashcardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompTIAFlashcardsTests.swift
+//  CompTIAFlashcardsTests
+//
+//  Created by connerjordan on 11/14/24.
+//
+
+import Testing
+@testable import CompTIAFlashcards
+
+struct CompTIAFlashcardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
